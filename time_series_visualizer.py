d97b932f549bc628d726f2f3874175221d846cc8
jamesra77 <james.amos7@gmail.com>
2025-01-08 06:23:24 -0500

    finished line chart, started working on bar chart

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,29 +5,34 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv("fcc-forum-pageviews.csv",
+                 parse_dates=True,
+                 index_col="date")
 
 # Clean data
-df = None
+outliers_mask = (df["value"] > df["value"].quantile(0.025)) & (df["value"] < df["value"].quantile(0.975))
+df = df[outliers_mask]
 
 
 def draw_line_plot():
     # Draw line plot
+    fig, axes = plt.subplots(figsize=(21, 6))
 
-
-
-
+    axes.plot(df, color="firebrick", linewidth=1.5)
+    axes.set_xlabel("Date")
+    axes.set_ylabel("Page Views")
+    axes.set_title("Daily freeCodeCamp Forum Page Views 5/2016-12/2019")
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
     return fig
 
 def draw_bar_plot():
+    # ***probably use sns.catplot() instead***
     # Copy and modify data for monthly bar plot
-    df_bar = None
-
+    df_bar = df.copy()
     # Draw bar plot
-
+    fig, axes = None
 
 
 
